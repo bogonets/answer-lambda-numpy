154ca642ab968efbb7cdb96f1069e0cf65cfdcc8
zer0 <osom8979@gmail.com>
2020-05-28 21:33:12 +0900

    Trivial commit.

--- numpy_zeros.app.py
+++ numpy_zeros.app.py
@@ -3,27 +3,18 @@
 
 import numpy as np
 
-DTYPE_INT8_NAME = "int8"
-DTYPE_INT16_NAME = "int16"
-DTYPE_INT32_NAME = "int32"
-DTYPE_INT64_NAME = "int64"
-DTYPE_UINT8_NAME = "uint8"
-DTYPE_UINT16_NAME = "uint16"
-DTYPE_UINT32_NAME = "uint32"
-DTYPE_UINT64_NAME = "uint64"
-DTYPE_FLOAT32_NAME = "float32"
-DTYPE_FLOAT64_NAME = "float64"
+
 DTYPE_NAME_TO_TYPE = {
-    DTYPE_INT8_NAME: np.int8,
-    DTYPE_INT16_NAME: np.int16,
-    DTYPE_INT32_NAME: np.int32,
-    DTYPE_INT64_NAME: np.int64,
-    DTYPE_UINT8_NAME: np.uint8,
-    DTYPE_UINT16_NAME: np.uint16,
-    DTYPE_UINT32_NAME: np.uint32,
-    DTYPE_UINT64_NAME: np.uint64,
-    DTYPE_FLOAT32_NAME: np.float32,
-    DTYPE_FLOAT64_NAME: np.float64,
+    "int8": np.int8,
+    "int16": np.int16,
+    "int32": np.int32,
+    "int64": np.int64,
+    "uint8": np.uint8,
+    "uint16": np.uint16,
+    "uint32": np.uint32,
+    "uint64": np.uint64,
+    "float32": np.float32,
+    "float64": np.float64,
 }
 DTYPE_TYPE_TO_NAME = {v: k for k, v in DTYPE_NAME_TO_TYPE.items()}
 
@@ -47,26 +38,10 @@ def on_get(key):
         return DTYPE_TYPE_TO_NAME[dtype]
 
 
-def on_create():
-    return True
-
-
-def on_init():
-    return True
-
-
-def on_valid():
-    return True
-
-
 def on_run():
     return {
         "result": np.zeros(shape, dtype)
     }
-
-
-def on_destroy():
-    return True
 
 
 if __name__ == '__main__':
---
+++ numpy_zeros.app.test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+def on_test():
+    return {
+        "case1": {
+        }
+    }
+
+
+if __name__ == '__main__':
+    pass
