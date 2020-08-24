83a8169676cac0d4bcad9bd9772177145ee661f7
zer0 <osom8979@gmail.com>
2020-08-24 18:18:53 +0900

    Fix: type error in numpy_item.py

--- numpy_item.app.py
+++ numpy_item.app.py
@@ -4,4 +4,4 @@ import numpy as np
 
 
 def on_run(array: np.ndarray, index: np.ndarray):
-    return {'result': array.item(index.tolist())}
+    return {'result': np.array([array.item(*index.tolist())], dtype=array.dtype)}
