aa5054331fa77bb0cf654d5727f748aa41bd23d2
zer0 <osom8979@gmail.com>
2020-08-25 09:35:27 +0900

    Create TestNumpyAll class.

--- tester.py
+++ tester.py
@@ -2,6 +2,7 @@
 
 import importlib.util
 import unittest
+import numpy as np
 
 
 def import_module(name):
@@ -11,59 +12,72 @@ def import_module(name):
     return module
 
 
-numpy_all = import_module('numpy_all')
-numpy_any = import_module('numpy_any')
-numpy_array = import_module('numpy_array')
-numpy_cmp = import_module('numpy_cmp')
-numpy_item = import_module('numpy_item')
-numpy_shape = import_module('numpy_shape')
-numpy_size = import_module('numpy_size')
-numpy_slice = import_module('numpy_slice')
-numpy_zeros = import_module('numpy_zeros')
+# noinspection PyCallingNonCallable
+class BaseLambdaTestCase:
+
+    def __init__(self, module_name):
+        self.lib = import_module(module_name)
+
+    def set(self, key, val):
+        if hasattr(self.lib, 'on_set'):
+            self.lib.on_set(key, val)
+
+    def set_dict(self, **kwargs):
+        for key, val in kwargs.items():
+            self.set(key, val)
+
+    def get(self, key):
+        if hasattr(self.lib, 'on_get'):
+            return self.lib.on_get(key)
+        else:
+            return None
+
+    def init(self):
+        if hasattr(self.lib, 'on_init'):
+            return self.lib.on_init()
+        else:
+            return True
+
+    def valid(self):
+        if hasattr(self.lib, 'on_valid'):
+            return self.lib.on_valid()
+        else:
+            return True
+
+    def run(self, *args, **kwargs):
+        assert hasattr(self.lib, 'on_run')
+        return self.lib.on_run(*args, **kwargs)
+
+    def destroy(self):
+        if hasattr(self.lib, 'on_destroy'):
+            self.lib.on_destroy()
 
 
-class TestNumpyLambdas(unittest.TestCase):
+class TestNumpyAll(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        pass
-
-    @classmethod
-    def tearDownClass(cls):
-        pass
+    def __init__(self, test_method_name='runTest'):
+        super().__init__(test_method_name)
+        self.module = BaseLambdaTestCase('numpy_array')
 
     def setUp(self):
-        pass
+        self.module.set_dict(elements='0,1,2,3,4,5', shape='2,3')
+        self.assertTrue(self.module.init())
+        self.assertTrue(self.module.valid())
 
     def tearDown(self):
-        pass
+        self.module.destroy()
 
-    def test_all(self):
-        self.assertTrue(True)
-
-    def test_any(self):
-        self.assertTrue(True)
-
-    def test_array(self):
-        self.assertTrue(True)
-
-    def test_cmp(self):
-        self.assertTrue(True)
-
-    def test_item(self):
-        self.assertTrue(True)
-
-    def test_shape(self):
-        self.assertTrue(True)
-
-    def test_size(self):
-        self.assertTrue(True)
-
-    def test_slice(self):
-        self.assertTrue(True)
-
-    def test_zeros(self):
-        self.assertTrue(True)
+    def test_default(self):
+        result = self.module.run()
+        self.assertIsInstance(result['result'], np.ndarray)
+        self.assertEqual((2, 3,), result['result'].shape)
+        self.assertEqual(6, result['result'].size)
+        self.assertEqual(0, result['result'][0, 0])
+        self.assertEqual(1, result['result'][0, 1])
+        self.assertEqual(2, result['result'][0, 2])
+        self.assertEqual(3, result['result'][1, 0])
+        self.assertEqual(4, result['result'][1, 1])
+        self.assertEqual(5, result['result'][1, 2])
 
 
 if __name__ == '__main__':
